ee977c19666d49d5d1838559a7e56deb40212dbc
Sami Abdullah <sami38596@gmailcom>
2026-05-15 23:49:55 +0600

    Did lots of work

---
+++ server/index.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const port = process.env.PORT || 5000
+
+const { MongoClient, ServerApiVersion } = require('mongodb')
+const dontenv = require('dotenv')
+dontenv.config()
+const uri = process.env.MONGODB_URI;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+app.use(cors())
+app.use(express.json())
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    
+    const db = client.db("travelast")
+    const destinationCollection = db.collection('destinations')
+    
+    
+    app.post('/destination', async (req, res) => {
+      const destination = req.body
+      if (destination)console.log(destination);
+      const result = await destinationCollection.insertOne(destination)
+      res.json(result)
+    })
+
+
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
